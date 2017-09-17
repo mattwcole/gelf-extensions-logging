1a7fe9af99192de86a701c9712501384b6f49935
Matt Cole <matt@matt-cole.co.uk>
2017-09-17 23:16:51 +0100

    Add sample projects - fixes #1

---
+++ samples/Gelf.Extensions.Logging.Samples.NetCore1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Gelf.Extensions.Logging.Samples.NetCore1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+
+            try
+            {
+                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+                loggerFactory
+                    .AddConsole(configuration.GetSection("Logging:Console"))
+                    .AddGelf(configuration.GetSection("Logging:GELF").Get<GelfLoggerOptions>());
+
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+                logger.LogInformation("Information level log from netcoreapp1.1");
+                logger.LogDebug("Debug level log from netcoreapp1.1");
+                logger.LogTrace("Trace level log from netcoreapp1.1");
+
+                using (logger.BeginScope(("custom_attribute", "12345")))
+                {
+                    logger.LogInformation("Log with custom attribute from netcoreapp1.1");
+                }
+            }
+            finally
+            {
+                // The LoggerFactory must be disposed before the program exits to ensure all queued messages are sent.
+                (serviceProvider as IDisposable)?.Dispose();
+            }
+        }
+    }
+}
---
+++ samples/Gelf.Extensions.Logging.Samples.NetCore2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Gelf.Extensions.Logging.Samples.NetCore2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var serviceCollection = new ServiceCollection()
+                .Configure<GelfLoggerOptions>(configuration.GetSection("Logging:GELF"))
+                .AddLogging(loggingBuilder => loggingBuilder
+                    .AddConfiguration(configuration.GetSection("Logging"))
+                    .AddConsole()
+                    .AddGelf());
+
+            // The LoggerFactory must be disposed before the program exits to ensure all queued messages are sent.
+            using (var serviceProvider = serviceCollection.BuildServiceProvider())
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+                logger.LogInformation("Information level log from netcoreapp2.0");
+                logger.LogDebug("Debug level log from netcoreapp2.0");
+                logger.LogTrace("Trace level log from netcoreapp2.0");
+
+                using (logger.BeginScope(("custom_attribute", "12345")))
+                {
+                    logger.LogInformation("Log with custom attribute from netcoreapp2.0");
+                }
+            }
+        }
+    }
+}
