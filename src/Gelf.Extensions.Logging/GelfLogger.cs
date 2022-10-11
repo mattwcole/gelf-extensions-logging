ce88595d50672642d5a66ca3f93846c1b2ea2711
Matt Cole <matt@matt-cole.co.uk>
2022-10-11 08:10:16 +0100

    Use custom NullScope class

--- src/Gelf.Extensions.Logging/GelfLogger.cs
+++ src/Gelf.Extensions.Logging/GelfLogger.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions.Internal;
 
 namespace Gelf.Extensions.Logging
 {
---
+++ src/Gelf.Extensions.Logging/NullScope.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gelf.Extensions.Logging;
+
+internal sealed class NullScope : IDisposable
+{
+    public static NullScope Instance { get; } = new();
+
+    private NullScope()
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+}
